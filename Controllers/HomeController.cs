c2078f3aac6f55ed6f962a732a2de4724cff9e71
DivyakumarPatel <83291936+DivyakumarPatel@users.noreply.github.com>
2021-04-27 17:25:42 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace S1G6_SISAPP.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-2OB06DM;Initial Catalog=SIS;Integrated Security=True;Pooling=False");
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
